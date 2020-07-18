36c8ff45804e3c36872181e470feef7c76f3aabc
Taylor Palmer <taylorjadepalmer@gmail.com>
2020-07-17 17:04:13 -0700

    setup, npm i, filestructure, completed router and restrict mw, added .env file. writing tests now

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -1,11 +1,64 @@
-const router = require('express').Router();
+const express = require("express");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const Users = require("./users-model");
 
-router.post('/register', (req, res) => {
-  // implement registration
+const router = require("express").Router();
+
+router.post("/register", async (req, res, next) => {
+  try {
+    const { username, password, department } = req.body;
+    const user = await Users.findBy({ username }).first();
+
+    if (user) {
+      return res.status(409).json({
+        message: "Username is already taken",
+      });
+    }
+
+    const newUser = await Users.add({
+      username,
+      password: await bcrypt.hash(password, 15),
+      department,
+    });
+
+    res.status(201).json(newUser);
+  } catch (err) {
+    next(err);
+  }
 });
 
-router.post('/login', (req, res) => {
-  // implement login
+router.post("/login", async (req, res, next) => {
+  try {
+    const { username, password } = req.body;
+    const user = await Users.findBy({ username }).first();
+
+    if (!user) {
+      return res.status(401).json({
+        message: "You shall not pass!",
+      });
+    }
+
+    const passwordValid = await bcrypt.compare(password, user.password);
+
+    if (!passwordValid) {
+      return res.status(401).json({
+        message: "You shall not pass!",
+      });
+    }
+
+    const payload = {
+      userId: user.id,
+      username: user.username,
+    };
+
+    res.json({
+      token: jwt.sign(payload, process.env.JWT_SECRET),
+      message: `Welcome ${user.username}!`,
+    });
+  } catch (err) {
+    next(err);
+  }
 });
 
 module.exports = router;
--- jokes/jokes-router.js
+++ jokes/jokes-router.js
@@ -1,19 +1,20 @@
-const axios = require('axios');
+const axios = require("axios");
+const restrict = require("../auth/restrict");
 
-const router = require('express').Router();
+const router = require("express").Router();
 
-router.get('/', (req, res) => {
+router.get("/", restrict(), async (req, res, next) => {
   const requestOptions = {
-    headers: { accept: 'application/json' },
+    headers: { accept: "application/json" },
   };
 
   axios
-    .get('https://icanhazdadjoke.com/search', requestOptions)
-    .then(response => {
+    .get("https://icanhazdadjoke.com/search", requestOptions)
+    .then((response) => {
       res.status(200).json(response.data.results);
     })
-    .catch(err => {
-      res.status(500).json({ message: 'Error Fetching Jokes', error: err });
+    .catch((err) => {
+      res.status(500).json({ message: "Error Fetching Jokes", error: err });
     });
 });
 
